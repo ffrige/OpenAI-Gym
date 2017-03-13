0111fc8539f13d6bf1628ea95e94363172148aba
ffrige <ffrige@hotmail.com>
2017-03-13 13:13:34 +0800

    clean up code

--- CartPole-v0/CartPolePG.py
+++ CartPole-v0/CartPolePG.py
@@ -6,8 +6,8 @@ from gym import wrappers
 
 import keras
 from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation
-from keras.optimizers import SGD, Adam, RMSprop
+from keras.layers.core import Dense, Activation
+from keras.optimizers import RMSprop
 from keras.utils import np_utils
 
 #initialize neural network to store policy
@@ -19,10 +19,7 @@ ActorNet.compile(loss='mse',optimizer='RMSprop',metrics=['mae'])
 
 num_episodes = 500
 
-memory_index = 0
-memory_full = 1
 memory_max = 200 #number of examples stored in memory (one episode)
-memory = np.empty([memory_max,7]) # 4 states - 2 actions values - 1 selected action
 
 #load environment
 env = gym.make('CartPole-v0')
@@ -38,7 +35,7 @@ for episode in range(num_episodes):
 
     loss = 0
     EpisodeReward = 0
-    episode_mem = np.empty([memory_max,7])
+    episode_mem = np.empty([memory_max,7]) # 4 states - 2 actions values - 1 selected action
 
     for t in range(300):
 
@@ -47,15 +44,13 @@ for episode in range(num_episodes):
 
         #evaluate NN to find Q-values for current state
 
-        #normalize input to NN
+        #normalize input values
         observation[0] /= 2.5
         observation[1] /= 2.5
         observation[2] /= 0.2
         observation[3] /= 2.5
         
         ActionValue = ActorNet.predict(observation.reshape(1,4),verbose=0).reshape(2,)
-
-        #act = np.argmax(ActionValue)
 
         #select best action (eps-greedy)
         eps = 1 - (episode / num_episodes)
@@ -89,28 +84,18 @@ for episode in range(num_episodes):
 
     #update finished episode memory with new reward
     #only update action value for actions that were taken, leave others unchanged
-    #TODO - vectorize!!!
+    #TODO - vectorize loop?
     alpha = 0.1
     for i in range (t+1):
         episode_mem[i,int(episode_mem[i,6])+4] = episode_mem[i,int(episode_mem[i,6])+4] *(1-alpha) + EpisodeReward * alpha
 
-    #put episode into memory pool (experience replay)
-    #TODO
-    """    
-    memory_index += 1
-    if memory_index >= memory_max:
-        memory_index = 0
-        memory_full = 1
-    """
-
-    #update weights of NN based on random picks from memory targets
-    if memory_full:
-        batch = np.empty([t+1,7])
-        for i in range(t+1):
-            batch[i] = episode_mem[i]
-        batch_in = batch[:,[0,1,2,3]]   #state
-        batch_tar = batch[:,[4,5]]    #action value
-        loss += ActorNet.train_on_batch(batch_in, batch_tar)[0]
+    #update weights of NN
+    batch = np.empty([t+1,7])
+    for i in range(t+1):
+        batch[i] = episode_mem[i]
+    batch_in = batch[:,[0,1,2,3]]   #input states
+    batch_tar = batch[:,[4,5]]    #target Q-values
+    loss += ActorNet.train_on_batch(batch_in, batch_tar)[0]
 
     print('Episode {0}, reward = {1}'.format(episode,EpisodeReward))
 
