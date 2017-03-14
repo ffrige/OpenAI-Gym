569f29e2ba3fe790df1d4388c3a85d482db17cce
ffrige <ffrige@hotmail.com>
2017-03-14 13:13:17 +0800

    updated to gym
    Added some comments and monitor wrapper

--- CartPole-v0/CartPolePG.py
+++ CartPole-v0/CartPolePG.py
@@ -1,3 +1,16 @@
+"""
+Solves the cartpole-v0 enviroment on OpenAI gym using policy search
+
+A neural network is used to store the policy
+
+At the end of each episode the target value for each taken action is
+updated with the total normalized reward (up to a learning rate)
+
+Then a standard supervised learning backprop on the entire batch is
+executed
+
+"""
+
 import numpy as np
 import numpy.matlib 
 
@@ -17,34 +30,38 @@ ActorNet.add(Dense(200,init='he_normal',activation='relu'))
 ActorNet.add(Dense(2,init='he_normal',activation='sigmoid'))
 ActorNet.compile(loss='mse',optimizer='RMSprop',metrics=['mae'])
 
-num_episodes = 500
-
-memory_max = 200 #number of examples stored in memory (one episode)
+NumEpisodes = 200
 
 #load environment
 env = gym.make('CartPole-v0')
-env = wrappers.Monitor(env, '/tmp/CartPolev0-1')
+env = gym.wrappers.Monitor(env, 'monitor')
 
 TotalReward = 0
+BufferSize = 0
+eps = 1
 
 #start learning
-for episode in range(num_episodes):
+for episode in range(NumEpisodes):
 
     #initial state
     observation = env.reset() #observe initial state
 
+    States = []
+    ActionValues = []
+    Actions = []
+
+    t = 0
     loss = 0
     EpisodeReward = 0
-    episode_mem = np.empty([memory_max,7]) # 4 states - 2 actions values - 1 selected action
 
-    for t in range(300):
-
+    while True:
+        
         #show graphical environment
-        env.render()
+        #env.render()
 
-        #evaluate NN to find Q-values for current state
+        #evaluate NN to find action probabilities for current state
 
-        #normalize input values
+        #normalize inputs
         observation[0] /= 2.5
         observation[1] /= 2.5
         observation[2] /= 0.2
@@ -52,29 +69,29 @@ for episode in range(num_episodes):
         
         ActionValue = ActorNet.predict(observation.reshape(1,4),verbose=0).reshape(2,)
 
-        #select best action (eps-greedy)
-        eps = 1 - (episode / num_episodes)
-        if eps<0.1:
-            eps = 0.1
+        #select best action eps-greedy with decay
+        eps -= 0.002
+        if eps<0:
+            eps = 0
         greedy = np.random.random()
         if greedy < eps:
-            act = np.random.randint(2)
+            Action = np.random.randint(2)
         else:
-            act = np.argmax(ActionValue)
+            Action = np.argmax(ActionValue)
         
         #execute action
-        observation_new, reward, done, info = env.step(act)
+        observation_new, reward, done, info = env.step(Action)
 
-        #normalize reward
+        #normalize reward, maximum reward per episode is 200
         reward /= 200.0
 
         EpisodeReward += reward
         
-        #save movement in memory to assign rewards at end of episode
-        episode_mem[t][0:4] = observation
-        episode_mem[t][4:6] = ActionValue
-        episode_mem[t][6] = act
-        
+        #save current movement in memory to assign rewards at end of episode
+        States.append(observation)
+        ActionValues.append(ActionValue)
+        Actions.append(Action)
+
         #update state
         observation = observation_new
 
@@ -82,19 +99,21 @@ for episode in range(num_episodes):
         if done:
             break
 
+        #next time step
+        t += 1
+
     #update finished episode memory with new reward
     #only update action value for actions that were taken, leave others unchanged
-    #TODO - vectorize loop?
     alpha = 0.1
-    for i in range (t+1):
-        episode_mem[i,int(episode_mem[i,6])+4] = episode_mem[i,int(episode_mem[i,6])+4] *(1-alpha) + EpisodeReward * alpha
-
-    #update weights of NN
-    batch = np.empty([t+1,7])
     for i in range(t+1):
-        batch[i] = episode_mem[i]
-    batch_in = batch[:,[0,1,2,3]]   #input states
-    batch_tar = batch[:,[4,5]]    #target Q-values
+        ActionValues[i][Actions[i]] = ActionValues[i][Actions[i]] * (1-alpha) + EpisodeReward * alpha
+
+    #update weights of NN based on last completed episode
+    batch_in = np.empty([t+1,4]) #input state
+    batch_tar = np.empty([t+1,2]) #target action values
+    for i in range(t+1):
+        batch_in[i] = States[i]
+        batch_tar[i] = ActionValues[i]
     loss += ActorNet.train_on_batch(batch_in, batch_tar)[0]
 
     print('Episode {0}, reward = {1}'.format(episode,EpisodeReward))
@@ -102,4 +121,6 @@ for episode in range(num_episodes):
     TotalReward += EpisodeReward
 
 print('Total reward = {0}'.format(TotalReward))
-ActorNet.save('CPv0_model.h5')
+#ActorNet.save('CPv0_model.h5')
+
+env.close()
